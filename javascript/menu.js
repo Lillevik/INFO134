48bbc5d66e765b27f0c4d02c8e3c8e4ffa9623b8
Lillevik <mariusfalch@hotmail.com>
2017-01-31 12:10:50 +0100

    Menu fix

--- javascript/menu.js
+++ javascript/menu.js
@@ -4,11 +4,14 @@
 
 
 function show_menu(){
-    var mobile_nav = $(".mobile-navigation");
+    var mobile_nav = $(".menu-list");
     if(mobile_nav.hasClass('open')){
         mobile_nav.removeClass('open');
+        mobile_nav.css('right','-200px');
     }else{
         mobile_nav.addClass('open');
+        mobile_nav.css('right','0');
     }
-
 }
+
+
