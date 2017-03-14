44d2084ace1f555cdfa2fdb6768427c362245fdc
Eirik Sjøvoll <eirik.sjovoll@student.uib.no>
2017-03-14 13:45:58 +0100

    removed old useless files and code

--- javascript/show_movie.js
+++ javascript/show_movie.js
@@ -3,19 +3,6 @@ function panic(message) {
     alert(message);
 }
 
-// function add_row(table, left, right) {
-//    new_row = document.createElement("TR");
-//    left_cell = document.createElement("TD");
-//     left_cell.appendChild(left);
-//     new_row.appendChild(left_cell);
-//    
-//    right_cell = document.createElement("TD");
-//     right_cell.appendChild(right);
-//     new_row.appendChild(right_cell);
-//     
-//     table.appendChild(new_row);
-// }
-
 window.onload = function() {
     query_params = get_query_string_parameters();
     if (!query_params.id) {
@@ -58,40 +45,5 @@ window.onload = function() {
     year_element.innerHTML = "Year: " + movie_object["year"];
     length_element.innerHTML = movie_object["length"] + " min";
     summary_element.innerHTML = movie_object["description"];
-    
-    // add a "debug-table" on the bottom showing all elements from movie_object
-    // stats_table = document.getElementById("movie_stat_table");
-    // for (key in movie_object) {
-    //     left = document.createTextNode(key);
-    //     right = document.createTextNode(movie_object[key]);
-    //     add_row(stats_table, left, right);
-    // }
-    
-    // add a "debug-table" on the bottom showing all genre info
-    // genre_table = document.getElementById("genre_stat_table");
-    // for (var i in genre_object) {
-	// 	left = document.createTextNode(i);
-	// 	right = document.createTextNode(genre_object[i]);
-	// 	add_row(genre_table, left, right);
-    // }
-
-    // review object debug-table
-    // review_table = document.getElementById("review_stat_table");
-    // for (key in review_object) {
-	// left = document.createTextNode(key);
-	// right = document.createTextNode(review_object[key]);
-	// add_row(review_table, left, right);
-	// for (subkey in review_object[key]) {
-	//     left = document.createTextNode(" -> " + subkey);
-	//     right = document.createTextNode(review_object[key][subkey]);
-	//     add_row(review_table, left, right);
-	// }
-    // }
-
-    
-    // var imageUrls = get_multiple_images(query_params.id, 3);
-    // for (var i = 0; i < imageUrls.length; i++) {
-    //     url = imageUrls[i];
-    //     append_image(url);
-    //}
+    document.title = movie["otitle"];
 };
