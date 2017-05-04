302aa99e5c37d320acd7e9ca26f182a52fdcb8bf
svimanet <eirik.sjovoll@student.uib.no>
2017-05-04 09:08:30 +0200

    categories done

--- javascript/ratings.js
+++ javascript/ratings.js
@@ -2,7 +2,6 @@ function get_rating(id){
     // Get the reviews
     rating_object = reviews_object[id];
     if (!rating_object) {
-        console.log("Movie " + id) + " has no reviews.";
         return;
     }
 
@@ -13,7 +12,6 @@ function get_rating(id){
         var value = rating_object[key];
         sum = sum + value.rating;
         num = num + 1;
-        console.log(value.rating)
     }
     var rating = Math.round((sum / num) * 10) / 10;
     return rating + " / 5";
